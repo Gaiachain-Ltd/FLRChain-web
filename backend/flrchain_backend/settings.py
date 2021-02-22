fb8d3fa80960d692e766e891097471556231cbf1
Damian Cholewa <dcholewa@milosolutions.com>
2021-02-22 11:50:31 +0100

    refs: #92076. Allowed hosts.

--- backend/flrchain_backend/settings.py
+++ backend/flrchain_backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'sEk2#K1izjzajAAk22kalsiAAAjzakl')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '195.201.81.231']
 
 
 # Application definition
