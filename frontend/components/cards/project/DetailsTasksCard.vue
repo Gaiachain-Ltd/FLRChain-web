9698a8ab8b691b2e3f4c4e72bbc7344eee537211
Damian Cholewa <dcholewa@milosolutions.com>
2021-03-15 11:14:03 +0100

    Improved UI of tasks details.

--- frontend/components/cards/project/DetailsTasksCard.vue
+++ frontend/components/cards/project/DetailsTasksCard.vue
@@ -1,7 +1,13 @@
 <template>
   <v-card ma-0>
     <v-layout column ma-0 pa-6>
-      <v-layout column ma-0 v-for="task in tasks" :key="task.id">
+      <v-flex mb-6>
+        <DefaultText :color="$vuetify.theme.themes.light.primary"
+          >Tasks:</DefaultText
+        >
+      </v-flex>
+      <v-layout column ma-0 v-for="(task, index) in tasks" :key="task.id">
+        <v-divider v-if="index" class="my-3"></v-divider>
         <DetailsTaskDelegate :task="task"></DetailsTaskDelegate>
       </v-layout>
     </v-layout>
@@ -16,6 +22,7 @@ export default {
   components: {
     DetailsTaskDelegate: () =>
       import("@/components/delegates/DetailsTaskDelegate"),
+    DefaultText: () => import("@/components/texts/DefaultText"),
   },
 };
 </script>
--- frontend/components/delegates/DetailsTaskDelegate.vue
+++ frontend/components/delegates/DetailsTaskDelegate.vue
@@ -1,20 +1,20 @@
 <template>
   <v-layout column ma-0>
-    <v-layout row ma-0>
+    <v-layout row mb-3 ma-0>
       <v-flex>
         <DefaultText>Action:</DefaultText>
       </v-flex>
       <v-spacer></v-spacer>
-      <v-flex>
+      <v-flex shrink>
         <DefaultText>{{ task.action }}</DefaultText>
       </v-flex>
     </v-layout>
-    <v-layout row ma-0>
+    <v-layout row ma-0 align-end>
       <v-flex>
         <DefaultText>Reward:</DefaultText>
       </v-flex>
       <v-spacer></v-spacer>
-      <v-flex>
+      <v-flex shrink>
         <DefaultText>{{ task.reward }}</DefaultText>
       </v-flex>
     </v-layout>
