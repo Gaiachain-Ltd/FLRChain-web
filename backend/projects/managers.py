329d6ab62e199f89e6bd007e6f0bd6ab7785f366
Damian Cholewa <dcholewa@milosolutions.com>
2021-03-19 13:46:58 +0100

    Fixed beneficiary query.

--- backend/projects/managers.py
+++ backend/projects/managers.py
@@ -1,9 +1,10 @@
+import projects.models
 from django.db import models
 
 
 class ProjectManager(models.Manager):
     def with_beneficiary_assignment_status(self, beneficiary):
-        return self.annotate(assignment_status=models.Case(
-            models.When(beneficiaries=beneficiary, then=models.F(
-                'beneficiaries__assignment__status'))
-        )).order_by('id', 'assignment_status').distinct('id')
+        assignments = projects.models.Assignment.objects.filter(
+            beneficiary=beneficiary,
+            project=models.OuterRef('pk'))
+        return self.annotate(assignment_status=models.Subquery(assignments.values('status')[:1]))
