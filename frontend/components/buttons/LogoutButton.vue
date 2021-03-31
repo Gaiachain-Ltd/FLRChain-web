d8c14f2bb0981129be74c73537c4dee73981ac43
Damian Cholewa <dcholewa@milosolutions.com>
2021-03-31 15:51:30 +0200

    Logout popup.

--- frontend/components/buttons/LogoutButton.vue
+++ frontend/components/buttons/LogoutButton.vue
@@ -1,25 +1,52 @@
 <template>
-  <v-layout
-    class="pointer-cursor"
-    row
-    shrink
-    ma-0
-    pa-0
-    align-center
-    @click.prevent="logOut"
-  >
-    <v-layout column ma-0>
-      <DefaulText :color="$vuetify.theme.themes.light.quinary">{{
-        `${$auth.user.first_name} ${$auth.user.last_name}`
-      }}</DefaulText>
-      <div class="account-type-text">{{ accountType }}</div>
-    </v-layout>
-    <v-flex class="logout-icon-style" shrink ml-4>
+  <DefaultPopup :show.sync="showLogoutPopup">
+    <template v-slot="{ on, attrs }">
+      <v-layout
+        class="pointer-cursor"
+        row
+        shrink
+        ma-0
+        pa-0
+        align-center
+        v-on="on"
+      >
+        <v-layout column ma-0>
+          <DefaulText :color="$vuetify.theme.themes.light.quinary">{{
+            `${$auth.user.first_name} ${$auth.user.last_name}`
+          }}</DefaulText>
+          <div class="account-type-text">{{ accountType }}</div>
+        </v-layout>
+        <v-flex class="logout-icon-style" shrink ml-4>
+          <DefaultSVGIcon
+            :icon="require('@/assets/toolbar/logout.svg')"
+          ></DefaultSVGIcon>
+        </v-flex>
+      </v-layout>
+    </template>
+    <v-flex slot="icon">
       <DefaultSVGIcon
-        :icon="require('@/assets/toolbar/logout.svg')"
+        :icon="require('@/assets/popup/logout.svg')"
+        :size="70"
       ></DefaultSVGIcon>
     </v-flex>
-  </v-layout>
+    <v-flex slot="content" my-6>
+      <DefaultText
+        class="text-center"
+        :size="22"
+        color="#253F50"
+        family="open-sans"
+        >Are you sure you want to log out?</DefaultText
+      >
+    </v-flex>
+    <v-layout slot="buttons" column ma-0 style="width: 100%">
+      <v-flex mb-3>
+        <BlockButton color="error" @clicked="logOut">Log out</BlockButton>
+      </v-flex>
+      <v-flex>
+        <BlockButton class="cancel-style" color="white"  @clicked="cancel">Cancel</BlockButton>
+      </v-flex>
+    </v-layout>
+  </DefaultPopup>
 </template>
 
 <script>
@@ -27,11 +54,15 @@ import { mapGetters } from "vuex";
 
 export default {
   data() {
-    return {};
+    return {
+      showLogoutPopup: false,
+    };
   },
   components: {
     DefaulText: () => import("@/components/texts/DefaultText"),
     DefaultSVGIcon: () => import("@/components/icons/DefaultSVGIcon"),
+    DefaultPopup: () => import("@/components/popups/DefaultPopup"),
+    BlockButton: () => import("@/components/buttons/BlockButton"),
   },
   computed: {
     ...mapGetters(["isFacililator"]),
@@ -41,8 +72,12 @@ export default {
   },
   methods: {
     logOut() {
+      this.showLogoutPopup = false;
       this.$auth.logout().then(this.$router.push("/login"));
     },
+    cancel() {
+      this.showLogoutPopup = false;
+    }
   },
 };
 </script>
@@ -60,5 +95,8 @@ export default {
 }
 .pointer-cursor {
   cursor: pointer;
+}
+.cancel-style {
+  border: 1px black solid !important;
 }
 </style>
