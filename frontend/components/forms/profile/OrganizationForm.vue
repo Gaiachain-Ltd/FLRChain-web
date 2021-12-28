d1038fba6bed68165aa0561c2befe6b92d1e55fc
Damian Cholewa <damian.cholewa.choli@gmail.com>
2021-12-28 14:20:23 +0100

    Frontend: Combobox component.

--- frontend/components/forms/profile/OrganizationForm.vue
+++ frontend/components/forms/profile/OrganizationForm.vue
@@ -6,6 +6,12 @@
         placeholder="Please enter organization name..."
         :text.sync="organization.name"
       ></TextInput>
+      <Combobox
+        label="Organization type"
+        placeholder="Please select organization type..."
+        :value.sync="organizationType"
+        :items="organizationTypes"
+      ></Combobox>
       <TextInput
         label="Website"
         placeholder="Please enter website..."
@@ -36,13 +42,45 @@
 </template>
 
 <script>
+import _ from "lodash";
+
 export default {
   props: {
     organization: {},
   },
+  data() {
+    return {
+      organizationTypes: [
+        { text: "Profit", value: 0 },
+        { text: "Nonprofit", value: 1 },
+        { text: "Individual", value: 2 },
+        { text: "Goverment", value: 3 },
+        { text: "Other", value: 4 },
+      ],
+    };
+  },
+  computed: {
+    organizationType: {
+      get() {
+        const index = _.findIndex(this.organizationTypes, [
+          "value",
+          this.organization.organization_type,
+        ]);
+        if (index !== -1) {
+          return this.organizationTypes[index];
+        } else {
+          return null;
+        }
+      },
+      set(value) {
+        this.$set(this.organization, "organization_type", value.value);
+      },
+    },
+  },
   components: {
     TextInput: () => import("@/components/inputs/TextInput"),
     TextAreaInput: () => import("@/components/inputs/TextAreaInput"),
+    Combobox: () => import("@/components/inputs/Combobox"),
   },
 };
 </script>
---
+++ frontend/components/inputs/Combobox.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-layout column>
+    <DefaultText :size="12" :color="$vuetify.theme.themes.light.senary">{{
+      label
+    }}</DefaultText>
+    <v-combobox
+      flat
+      solo
+      height="50"
+      v-model="internalValue"
+      :placeholder="placeholder"
+      :items="items"
+      :background-color="$vuetify.theme.themes.light.tertiary"
+    ></v-combobox>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {},
+    label: {
+      type: String,
+      default: "",
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    error: {
+      type: Boolean,
+      default: false,
+    },
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    internalValue: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("update:value", value);
+      },
+    },
+  },
+  components: {
+    DefaultText: () => import("@/components/texts/DefaultText"),
+  },
+};
+</script>
