dddf50604a581d5f8dbd45b5072d37e631d6215f
Damian Cholewa <damian.cholewa.choli@gmail.com>
2022-02-08 15:55:55 +0100

    Upload project image.

---
+++ frontend/components/buttons/UploadButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-layout column shrink align-end>
+    <v-layout style="width: 100%" mb-3>
+      <DefaultText :size="12" :color="$vuetify.theme.themes.light.senary"
+        >Project image</DefaultText
+      >
+    </v-layout>
+    <v-img
+      :src="require('@/assets/images/placeholder.png')"
+      contain
+    ></v-img>
+    <v-layout shrink mt-3>
+      <ActionButton color="primary">Upload file</ActionButton>
+    </v-layout>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  components: {
+    DefaultText: () => import("@/components/texts/DefaultText"),
+    ActionButton: () => import("@/components/buttons/ActionButton"),
+  },
+};
+</script>
--- frontend/components/forms/project/ProjectForm.vue
+++ frontend/components/forms/project/ProjectForm.vue
@@ -1,58 +1,56 @@
 <template>
   <v-form ref="form">
     <v-layout column>
-      <v-layout row ma-0>
-        <v-flex xs12 sm12 md7 shrink>
-          <TextInput
-            label="Project title*"
-            placeholder="Please enter project name..."
-            v-model="project.title"
-            :rules="requiredRules"
-            required
-          ></TextInput>
+      <v-layout align-center>
+        <v-flex md5 class="mr-3">
+          <v-layout column>
+            <TextInput
+              label="Project title*"
+              placeholder="Please enter project name..."
+              v-model="project.title"
+              :rules="requiredRules"
+              required
+            ></TextInput>
+            <TextInput
+              label="FLR Action"
+              placeholder="Please enter project name..."
+              v-model="project.title"
+              :rules="requiredRules"
+              required
+            ></TextInput>
+            <TextInput
+              label="Fundraising duration"
+              v-model="project.fundraising_duration"
+              :rules="requiredRules"
+              required
+            ></TextInput>
+            <v-layout>
+              <DateInput
+                label="Start of project*"
+                :text.sync="project.start"
+                :rules="[...requiredRules, ...dateRules]"
+                required
+              ></DateInput>
+              <DateInput
+                label="End of project*"
+                :text.sync="project.end"
+                :rules="[...requiredRules, ...dateRules]"
+                required
+              ></DateInput>
+            </v-layout>
+          </v-layout>
         </v-flex>
-        <v-spacer></v-spacer>
-        <v-flex xs12 sm5 md2 shrink>
-          <DateInput
-            label="Start of project*"
-            :text.sync="project.start"
-            :rules="[...requiredRules, ...dateRules]"
-            required
-          ></DateInput>
-        </v-flex>
-        <v-spacer></v-spacer>
-        <v-flex xs12 sm5 md2 shrink>
-          <DateInput
-            label="End of project*"
-            :text.sync="project.end"
-            :rules="[...requiredRules, ...dateRules]"
-            required
-          ></DateInput>
+        <v-flex md7>
+          <v-layout justify-center>
+            <UploadButton></UploadButton>
+          </v-layout>
         </v-flex>
       </v-layout>
-      <v-layout>
-        <TextInput
-          label="Fundraising duration"
-          v-model="project.fundraising_duration"
-          :rules="requiredRules"
-          required
-        ></TextInput>
-      </v-layout>
-      <v-layout>
-        <TextAreaInput
-          label="Description"
-          placeholder="Please enter project description..."
-          v-model="project.description"
-        ></TextAreaInput>
-      </v-layout>
-      <v-layout>
-        <TextInput
-          label="Facilitator administration funds"
-          v-model="project.fac_adm_funds"
-          :rules="requiredRules"
-          required
-        ></TextInput>
-      </v-layout>
+      <TextAreaInput
+        label="Description"
+        placeholder="Please enter project description..."
+        v-model="project.description"
+      ></TextAreaInput>
     </v-layout>
   </v-form>
 </template>
@@ -66,9 +64,11 @@ export default {
     project: {},
   },
   components: {
+    DefaultText: () => import("@/components/texts/DefaultText"),
     TextInput: () => import("@/components/inputs/TextInput"),
     TextAreaInput: () => import("@/components/inputs/TextAreaInput"),
     DateInput: () => import("@/components/inputs/DateInput"),
+    UploadButton: () => import("@/components/buttons/UploadButton"),
   },
   methods: {
     validate() {
