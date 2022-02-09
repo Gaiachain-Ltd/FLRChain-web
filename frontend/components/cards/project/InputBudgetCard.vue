74c52ca1b3b0b5aa33b4b97fb2c41d3b26440e10
Damian Cholewa <damian.cholewa.choli@gmail.com>
2022-02-09 16:01:31 +0100

    Missing files.

---
+++ frontend/components/cards/project/InputBudgetCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-layout column class="border-wrapper pa-3">
+    <BudgetForm :project="project"></BudgetForm>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: {
+    project: {},
+  },
+  components: {
+    BudgetForm: () => import("@/components/forms/project/BudgetForm"),
+  },
+};
+</script>
---
+++ frontend/components/forms/project/BudgetForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-layout column>
+    <DefaultText class="mb-3" :color="$vuetify.theme.themes.light.primary"
+      >Budget</DefaultText
+    >
+    <TextInput
+      label="Facilitator administration funds"
+      v-model="project.fac_adm_funds"
+    ></TextInput>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: {
+    project: {},
+  },
+  components: {
+    DefaultText: () => import("@/components/texts/DefaultText"),
+    TextInput: () => import("@/components/inputs/TextInput"),
+  },
+};
+</script>
